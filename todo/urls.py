f16960577f380d3559fbe64d8a5f0aa6c2f0304e
Daniel-darnell <darnelldanny1997@gmail.com>
2020-12-09 09:03:51 +0300

    admin customization

---
+++ todo/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+#Django Admin page customization
+
+admin.site.site_header = 'Karibu Tenda'
+admin.site..site_title = 'Karibu Tenda Admin'
+admin.site.index_title = 'Welcome to Tenda Admin'
